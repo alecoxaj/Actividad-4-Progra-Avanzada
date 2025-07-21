a8df08948317d2af74e3b6c9e7f4f7039a08e1cd
Alejandro <alejandrocoxaj94@gmail.com>
2025-07-21 17:22:51 -0600

    Alejandro hizo el Ejercicio No. 1 de Simulador de votaciones

---
+++ 01.Simuladordevotación.py
@@ -0,0 +1,23 @@
+print("¡Bienvenido al Simulador!")
+name = input("Ingresa tu Nombre Completo: ")
+
+if len(name.strip()) < 5:
+    print("¡Nombre inválido! Debe tener al menos 5 letras")
+elif True:
+    DPI = input("Ingresa el No. de DPI (13 dígitos): ").strip()
+    if not DPI.isdigit() or len(DPI) != 13:
+        print("¡DPI Inválido! Debe tener al menos 13 dígitos")
+    elif True:
+        dep = input("Ingresa el Departamento: ").strip()
+        birthdate = int(input("Ingresa tu año de nacimiento:  "))
+
+        currentyear = 2025
+        age = currentyear - birthdate
+
+        if (dep == "Petén" or dep == "Alta Verapaz") and age >= 17:
+            print("¡Bienvenido", name +"!","tu centro de votación está en", dep + ".")
+        elif age >= 18:
+            print("¡Bienvenido", name +"!","tu centro de votación está en", dep + ".")
+        else:
+            print("No cumples con la edad mínima para votar.")
+
